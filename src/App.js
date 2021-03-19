1bd9adcbf4af5821f7435956d5de62a74ba45a08
jrauvola <jrauvola@gmail.com>
2021-03-19 06:22:22 -0700

    Done

--- src/App.js
+++ src/App.js
@@ -7,80 +7,82 @@ class App extends Component {
   state = {
     characters: [],
   }
+
+  makeDeleteCall(character, index){
+    return axios.delete('http://localhost:5000/users', { data: character })
+      .then(function (response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+  }
+
   removeCharacter = index => {
     const { characters } = this.state
-
-    this.makeDeleteCall(character).then( callResult => {
-       if (callResult === true) { 
-        this.setState({
-          characters: characters.filter((character, i) => {
-            return i !== index
-          }),
-        })
-      }
-    } 
+    
+    this.setState({
+      characters: characters.filter((character, i) => {
+        if(i === index){
+          this.makeDeleteCall(character, index).then( callResult => {
+            if (callResult){
+              return false;
+            }
+          })
+        }
+        else{
+          return true;
+        }
+      }),
+    })
   }
-  
 
-handleSubmit = character => {
-  this.makePostCall(character).then( callResult => {
-     if (callResult === true) {
-        this.setState({ characters: [...this.state.characters, character] });
-     }
-  });
-}
+  handleSubmit = character => {
+    this.makePostCall(character).then( callResult => {
+      if (callResult){
+        if (callResult.status === 201){
+          this.setState({ characters: [...this.state.characters, callResult.data.user] });
+        }
+      }
+    });
+  }
 
-makeDeleteCall(character){
-  return axios.delete('http://localhost:5000/users', character)
-     .then(function (response) {
-     console.log(response);
-     return (response.status === 200);
-   })
-   .catch(function (error) {
-     console.log(error);
-     return false;
-   });
-}
+  makePostCall(character){
+    return axios.post('http://localhost:5000/users', character)
+    .then(function (response) {
+      console.log(response);
+      return (response.status === 200);
+    })
+    .catch(function (error) {
+      console.log(error);
+      return false;
+    });
+  }
 
-makePostCall(character){
-  return axios.post('http://localhost:5000/users', character)
-   .then(function (response) {
-     console.log(response);
-     return (response.status === 200);
-   })
-   .catch(function (error) {
-     console.log(error);
-     return false;
-   });
-}
+  componentDidMount() {
+    axios.get('http://localhost:5000/users')
+    .then(res => {
+      const characters = res.data.users_list;
+      this.setState({ characters });
+    })
+    .catch(function (error) {
+      //Not handling the error. Just logging into the console.
+      console.log(error);
+    });
+  }
 
-componentDidMount() {
-  axios.get('http://localhost:5000/users')
-   .then(res => {
-     const characters = res.data.users_list;
-     this.setState({ characters });
-   })
-   .catch(function (error) {
-     //Not handling the error. Just logging into the console.
-     console.log(error);
-   });
-}
+  render() {
+    const { characters } = this.state
 
-render() {
-  <Form handleSubmit={this.handleSubmit} />
-  const { characters } = this.state
-
-  return (
-    <div className="container">
-       <Table characterData={characters} removeCharacter={this.removeCharacter} />
-       <Form handleSubmit={this.handleSubmit}/>
-    </div>
-  )
-}
-
-handleSubmit = character => {
-  this.setState({ characters: [...this.state.characters, character] })
-}
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit}/>
+      </div>
+    )
+  }
 
 }
 export default App
